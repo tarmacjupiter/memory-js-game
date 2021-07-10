be658c11afa2fc069b9c28cf3318f4c27eb99f9b
tarmacjupiter <beheryanthony@gmail.com>
2021-07-10 16:16:04 -0500

    onClick toggles numbers on all Cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/Card.css";
 
 function Board({ deck }) {
+  const [showResults, setShowResults] = useState(true);
+  const onClick = () => setShowResults(showResults ? false : true);
+
   return (
     <>
       {deck.map((numOfCards, index) => {
         return (
           <>
-            <div key={index} className="Card">
-              {numOfCards.content}
+            <div key={index} className="Card" onClick={onClick}>
+              {showResults ? numOfCards.content : null}
             </div>
-            ;
           </>
         );
       })}
